270f66d5081a7dbada73d03f26e187af7ad5399e
yen <yenthefirst@gmail.com>
2018-09-26 23:00:27 -0700

    test 4

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,10 @@
 class Bottles
+  def num_bottles(num)
+    (num > 0 ? num.to_s : 'no more') + ' ' + (num == 1 ? "bottle" : "bottles")
+  end
+
   def verse(num)
-    num_bottles = "#{num} bottle#{'s' if num > 1}"
-    fewer_bottles = "#{num-1} bottle#{'s' if (num-1) > 1}"
-    "#{num_bottles} of beer on the wall, #{num_bottles} of beer.\n" +\
-    "Take one down and pass it around, #{fewer_bottles} of beer on the wall.\n"
+    "#{num_bottles(num)} of beer on the wall, #{num_bottles(num)} of beer.\n" +\
+    "Take #{num == 1 ? 'it' : 'one'} down and pass it around, #{num_bottles(num-1)} of beer on the wall.\n"
   end
 end

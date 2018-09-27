59dbcffb22265fb7cf3a142ae9bbe222830f9895
yen <yenthefirst@gmail.com>
2018-09-26 23:05:21 -0700

    test 5

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -3,8 +3,20 @@ class Bottles
     (num > 0 ? num.to_s : 'no more') + ' ' + (num == 1 ? "bottle" : "bottles")
   end
 
+  def what_you_do(num)
+    case num
+    when 0 then "Go to the store and buy some more"
+    when 1 then "Take it down and pass it around"
+    else "Take one down and pass it around"
+    end
+  end
+
   def verse(num)
-    "#{num_bottles(num)} of beer on the wall, #{num_bottles(num)} of beer.\n" +\
-    "Take #{num == 1 ? 'it' : 'one'} down and pass it around, #{num_bottles(num-1)} of beer on the wall.\n"
+    next_num = num > 0 ? num-1 : 99
+
+    "#{num_bottles(num)} of beer on the wall, ".capitalize + \
+    "#{num_bottles(num)} of beer.\n" + \
+    what_you_do(num) +", "+ \
+    "#{num_bottles(next_num)} of beer on the wall.\n"
   end
 end
